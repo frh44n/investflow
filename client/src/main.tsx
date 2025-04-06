9a874a4d899d778f0d27b0ea8aca547fa953b888
frh44n <107492548+frh44n@users.noreply.github.com>
2025-04-06 08:45:56 +0530

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import './index.css';
+import './utilities.css'; // must come AFTER
 createRoot(document.getElementById("root")!).render(<App />);
